ae4face5b638bfe6353bfe951a7cf1409e4ac264
AnshhSingh <anhsingh25bd@gmail.com->
2022-07-27 02:10:42 +0530

    new file:   strings.c 	deleted:    array  Untracked files: 	arrays/

---
+++ strings.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    // char str[] = {'a','n','s','h','\0'};
+    char str[] = "ansh";
+    printf("%s", &str);
+    // char *ptr=&str[0];
+    //  for (int i = 0; i < 4
+
+    // ; i++)
+    // {
+    //     printf("%c",*ptr);
+    //     ptr++;
+    // }
+
+    return 0;
+}
