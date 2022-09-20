321cff99a648a520ae62334527960d72c28ffc51
Ansh <ANSHSINGH25BD@GMAIL.COM>
2022-09-20 08:53:08 -0700

    Update leap.c

--- leap.c
+++ leap.c
@@ -4,15 +4,14 @@ int main(){
     printf("enter year");
     scanf("%d",&year);
     
-    if (year%4==0 && year%400==0 && year%100==0)
+    if (year%4==0 && year%400==0)
     {
         printf("it is a leap year");
     }
+    else if(year % 100 == 0){
+        printf("not a leap year");
     else {
         printf("not leap year");
     }
-    
-
-    
     return 0;
 }
